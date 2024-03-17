132614be40b047d45c35c136f31ac80923f2bd3e
eduuh <eduuh.muraya@outlook.com>
2024-03-18 00:00:22 +0300

    Restructur

---
+++ content/workflows/_index.md
@@ -0,0 +1,13 @@
++++
+title = "Workflow"
+weight = 1
++++
+
+> ### Goals
+
+1. Reach 100 wpm. Typing speed. Current ( 50Wpm ).
+2. Improve my Keyboard centric workflow editing.
+
+> ### TMUX
+
+> ### NVIM
--- tina/config.js
+++ tina/config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
+const branch =
+  process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
 
 export default defineConfig({
   branch,
-  clientId: "cf05fea4-4f23-47cf-be81-bf18b623b233", // Get this from tina.io
-  token: "bdce3e17adfa4a16da812d6c538a22cd61dd057b", // Get this from tina.io
+  clientId: "3c27f429-7854-4620-8e73-85ed2a05d934",
+  token: "0f29a40f41afcb5550a54284047707d84a76700d", // Get this from tina.io
 
   build: {
     outputFolder: "admin",
@@ -38,12 +39,12 @@ export default defineConfig({
           {
             type: "number",
             name: "weight",
-            label: "Weight"
+            label: "Weight",
           },
           {
             type: "string",
             name: "description",
-            label: "Description"
+            label: "Description",
           },
           {
             type: "rich-text",
@@ -77,13 +78,13 @@ export default defineConfig({
             type: "string",
             name: "categories",
             label: "Categories",
-            list: true
+            list: true,
           },
           {
             type: "string",
             name: "tags",
             label: "Tags",
-            list: true
+            list: true,
           },
           {
             type: "datetime",
@@ -135,10 +136,10 @@ export default defineConfig({
   },
   search: {
     tina: {
-      indexerToken: "876018600e28b19feaf033cc3364f588fe8a6248",
-      stopwordLanguages: ["deu", "eng", "fra", "ita", "spa", "nld"]
+      indexerToken: "bf3652582dc04bff310ee9d2b386c5f52e043054",
+      stopwordLanguages: ["deu", "eng", "fra", "ita", "spa", "nld"],
     },
     indexBatchSize: 100,
-    maxSearchIndexFieldLength: 100
-  }
+    maxSearchIndexFieldLength: 100,
+  },
 });
